64b311b02fb02226c2ed7ff60d685b6f83e61c38
Sapata <157375460+Sapata8810@users.noreply.github.com>
2026-02-08 20:52:33 -0500

    Add files via upload

---
+++ ejercicio 1-2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static string FormatoHora(int segundosTotales)
+    {
+        int horas = segundosTotales / 3600;
+        int minutos = (segundosTotales % 3600) / 60;
+        int segundos = segundosTotales % 60;
+
+        return $"{horas:D2}:{minutos:D2}:{segundos:D2}";
+    }
+
+    static void Main(string[] args)
+    {
+        int entradaSegundos = 3665; // ejemplo: 3665 segundos
+        string resultado = FormatoHora(entradaSegundos);
+        Console.WriteLine(resultado); // salida: 01:01:05
+    }
+}
